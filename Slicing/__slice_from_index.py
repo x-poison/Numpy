de202bb25e1a4edccebd146e86287e4ffdaf7d45
x-poison <abdulrahmanmossadhacker@gmail.com>
2024-04-24 06:12:21 -0700

    slice from certain index

--- Slicing/__slice_from_index.py
+++ Slicing/__slice_from_index.py
@@ -6,5 +6,6 @@ arr = np.array([1,2,3,4,5,6,7,8,9])
 #slice an array
 sliced_Array = arr[2:]
 
+#The output will begin from the value of the from like here we have 2 so the output will be from index 2 which will be 3 in this case
 #print sliced Array
 print(sliced_Array)
