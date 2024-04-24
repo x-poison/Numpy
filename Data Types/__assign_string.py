aaf8a59cccc7146661ac501f5a1581e33798f678
x-poison <abdulrahmanmossadhacker@gmail.com>
2024-04-24 07:56:08 -0700

    Strings assigned

---
+++ Data Types/__assign_string.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+#Create Array of strings assigned to the
+arr = np.array([0,1,2,3,4,5], dtype='S')
+
+#Now try print the Array and see
+print(arr)
+#output => [b'0' b'1' b'2' b'3' b'4' b'5']
+
+#print data type of the arres
+print(arr.dtype)
+#output => |S1
