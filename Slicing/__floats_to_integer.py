2ba204a50a1ed216bbbd1d4a78d9a3dfa35a5fc1
x-poison <abdulrahmanmossadhacker@gmail.com>
2024-04-24 11:31:25 -0700

    __float to integer

--- Slicing/__floats_to_integer.py
+++ Slicing/__floats_to_integer.py
@@ -13,3 +13,12 @@ print(newArr)
 #print data type of newArr
 print(newArr.dtype)
 #output => int32
+
+#lets declare new Array and use int as parameter
+arr2 = np.array([1.2,3.2,1.5,3.7,4.5,5.7])
+newArr2 = arr2.astype(int)
+print(newArr2.dtype)
+#output => int32
+
+print(newArr2)
+#output => [1 3 1 3 4 5]
