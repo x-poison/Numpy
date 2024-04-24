19bd9b79c353feabad9ede3cd9a1d4af1d8b8a13
x-poison <abdulrahmanmossadhacker@gmail.com>
2024-04-24 07:50:44 -0700

    Strings

---
+++ Data Types/__strings.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#create array
+string_array = np.array(['Mouse', 'KeyBoard', 'Monitor', 'Ram Module', 'CPU'])
+
+#Datta types here
+data_type = string_array.dtype
+
+print(data_type)
+#output => <U10
